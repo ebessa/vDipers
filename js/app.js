3e30af99865135f76d01e1a0398a35f285504690
Erick Bessa <droidebessa@gmail.com>
2016-04-20 18:30:18 -0300

    altera url de permissao e acrescenta verificacoes para script nao quebrar

--- js/app.js
+++ js/app.js
@@ -1,39 +1,49 @@
 var menuIddle = true;
-var intvl = 1;
-
+var count = 0;
+console.log('roooooodei');
 function followHash(){
   var currentPath = window.location.hash.split('/');
 
-  currentPath.shift();
+  currentPath.shift(); // remove #
 
-  currentPath.forEach(function(a, i){console.log('test');
-      var clickInterval = setInterval(function () {console.log('interval', i, intvl);intvl++;
+  currentPath.forEach(function(a, i){
+      var clickInterval = setInterval(function () {
         if(menuIddle){
-          menuIddle = false;
 
           var clickable = $('.jqueryFileTree a').filter(function(){
-            console.log('this.text', this.text);
-            console.log("a.replace(/slash/, '/')", a.replace(/slash/, '/'));
             return this.text === a.replace(/slash/, '/');
           });
 
-          clickable.parent().on('DOMNodeInserted', function(){
-            menuIddle = true;
-            console.log('DOMNodeInserted');
-            clearInterval(clickInterval);
-          });
+          if(clickable.length === 1){
+              menuIddle = false;
+              clickable.parent().on('DOMNodeInserted', function(){
+              menuIddle = true;
+              clearInterval(clickInterval);
+              // console.log('DOMNodeInserted');
+              // console.warn('interValCleared');
+            });
 
-          clickable[0].click();// click nativo DOM. Click de jquery não funciona
-          HideBusy();//função da vtex
-
-          intvl--;
+            clickable[0].click();// click nativo DOM. Click de jquery não funciona
+            location.href="javascript:HideBusy(); void 0";//gambiarra pra executar função da vtex
+          }
         }
       }, 100);
   });
 }
 
 function clickListener(){console.log('clickListener');
-  var url;
+  var url,
+      interval;
+
+  interval = setInterval(function () {
+    if($('.vtex-portal-main a').length === 0){
+      clickListener();
+      return;
+    }
+    console.log('cleared');
+    clearInterval(interval);
+  },100);
+
   $('.vtex-portal-main a')
   .unbind('click.extension')
   .on('click.extension', function(e){
@@ -46,6 +56,7 @@ function clickListener(){console.log('clickListener');
     .addClass('ext-level-' + (parseInt(len, 10)-1));
 
     $(this).parents('.tree-holder').each(function(i,el){
+      console.log("$(el).attr('ext-path')", $(el).attr('ext-path'))
       urlTable.push($(el).attr('ext-path'));
     });
 
@@ -59,9 +70,8 @@ $(function(){
   $('.vtex-portal-main .jqueryFileTree')
   .addClass('tree-holder')
   .addClass('ext-level-0');
+  clickListener();
   followHash();
 });
 
 $(window).on('hashchange', followHash);// isso só será disparado quando o usuario clicar no "back" button
-
-clickListener();
